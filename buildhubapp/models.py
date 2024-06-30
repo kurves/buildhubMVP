7024bfe8105475dffefd48595d446ab205637363
kurves <kavssimply@gmail.com>
2024-06-30 19:56:59 +0300

    add project model to app

--- buildhubapp/models.py
+++ buildhubapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
