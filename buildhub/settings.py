219cede51000a156dc1703d902d6641a5ed6c17d
kurves <kavssimply@gmail.com>
2024-07-05 20:10:43 +0300

    update settings file

--- buildhub/settings.py
+++ buildhub/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'buildhubapp',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
