d029009d9e56f1eddf84e240c168f2c60793f171
kurves <kavssimply@gmail.com>
2024-07-09 17:01:41 +0300

    urls to connct pages

--- buildhubapp/urls.py
+++ buildhubapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, MessageViewSet, UserViewSet
+
+
 
 urlpatterns = [
     path('', views.home, name='home'),
